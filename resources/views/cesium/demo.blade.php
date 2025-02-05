2575a2e23b08d53f6964be640c4575ece872fbe7
HaretaSK <andy19991214@gmail.com>
2025-02-05 16:29:40 +0800

    incomplete - sync height to waypoints array when input box changed

--- resources/views/cesium/demo.blade.php
+++ resources/views/cesium/demo.blade.php
@@ -1,5 +1,6 @@
 <script src="/vendor/CesiumUnminified/Cesium.js"></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <link rel="stylesheet" href="/vendor/CesiumUnminified/Widgets/widgets.css" />
 
 <style>
@@ -13,12 +14,14 @@
 
     #toolbox {
         position: absolute;
+        padding-left: 0.5rem;
         top: 5px;
         left: 5px;
-        width: 30%;
+        width: 35%;
         height: 20%;
         background-color: white;
         overflow: scroll;
+
     }
     #toolbox th, #toolbox td {
         padding-right: 0.5rem;
@@ -43,7 +46,7 @@
                 <td data-bind="text: $index"></td>
                 <td data-bind="text: longitude"></td>
                 <td data-bind="text: latitude"></td>
-                <td><input type="text" data-bind="textInput: height" style="width: 2rem;"></td>
+                <td><input type="text" data-bind="textInput: height" style="width: 3.5rem;"></td>
             </tr>
         </tbody>
     </table>
@@ -53,17 +56,50 @@
     window.CESIUM_BASE_URL = '/vendor/CesiumUnminified';
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNDczMzZmOS1kNDgxLTRkOGQtYTY0Mi0xMzVjNjZiZTdjNmQiLCJpZCI6MjczMjUwLCJpYXQiOjE3Mzg2NTY1NTB9.A5VQBmzdB-kyb75qWpyC4Q5iO8WOARHFqeiE_hjksz0';
     const viewer = new Cesium.Viewer('viewer');
-    const scene = viewer.scene;
-    if (!scene.pickPositionSupported) {
-        window.alert("This browser does not support pickPosition.");
-    }
     const pinBuilder = new Cesium.PinBuilder();
 
     let handler;
-    let cartesian;
+    let currentCoordinate;
     let waypointsArray = ko.observableArray();
+    let path;
 
     ko.applyBindings({ waypointsArray }, document.getElementById('toolbox'));
+
+    function renderPath(){
+        if (path) {
+            viewer.entities.remove(path);
+        }
+        if (waypointsArray().length > 1) {
+            const waypointsRenderArray = [];
+            waypointsArray().forEach(waypoint => {
+                waypointsRenderArray.push(
+                    waypoint.longitude,
+                    waypoint.latitude,
+                    waypoint.height
+                );
+            });
+
+            waypointsRenderArray.push(
+                waypointsArray()[0].longitude,
+                waypointsArray()[0].latitude,
+                waypointsArray()[0].height
+            );
+
+            path = viewer.entities.add({
+                polyline: {
+                    positions: Cesium.Cartesian3.fromDegreesArrayHeights(waypointsRenderArray),
+                    width: 5,
+                    material: Cesium.Color.GREEN,
+                    clampToGround: false,
+                },
+            });
+        }
+    }
+
+    function clearAll() {
+        viewer.entities.removeAll();
+        waypointsArray.removeAll();
+    }
 
     const coordBox = viewer.entities.add({
         label: {
@@ -78,15 +114,15 @@
 
     handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
     handler.setInputAction(movement => {
-        cartesian = viewer.camera.pickEllipsoid(
+        currentCoordinate = viewer.camera.pickEllipsoid(
             movement.endPosition,
-            scene.globe.ellipsoid,
+            viewer.scene.globe.ellipsoid,
         );
-        if (cartesian) {
-            coordBox.position = cartesian;
-            const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-            const longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(5);
-            const latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(5);
+        if (currentCoordinate) {
+            coordBox.position = currentCoordinate;
+            const cartographic = Cesium.Cartographic.fromCartesian(currentCoordinate);
+            const longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(6);
+            const latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(6);
             coordBox.label.text = `${longitudeString}\n${latitudeString}`;
             coordBox.label.show = true;
         } else {
@@ -95,24 +131,73 @@
     }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 
     handler.setInputAction(position => {
-        if (cartesian) {
-            const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-            const longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(5);
-            const latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(5);
-            waypointsArray.push({
-                longitude: longitudeString,
-                latitude: latitudeString,
-                height: 50
-            });
-            viewer.entities.add({
-                position: cartesian,
+        if (currentCoordinate) {
+            const cartographic = Cesium.Cartographic.fromCartesian(currentCoordinate);
+            const longitude = Cesium.Math.toDegrees(cartographic.longitude);
+            const latitude = Cesium.Math.toDegrees(cartographic.latitude);
+            const height = 1500;
+            const billboard = viewer.entities.add({
+                position: currentCoordinate,
                 billboard: {
-                    image: pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
+                    image: pinBuilder.fromColor(Cesium.Color.BLUE, 48).toDataURL(),
                     verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
                 }
             })
+
+            waypointsArray.push({
+                longitude: longitude,
+                latitude: latitude,
+                height: height,
+                billboard_id: billboard.id
+            });
         }
-    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+
+    document.addEventListener(
+        "keydown",
+        (event) => {
+            switch (event.key) {
+                case "Backspace":
+                    if(viewer.selectedEntity) {
+                        if(viewer.selectedEntity.billboard){
+                            const billboardId = viewer.selectedEntity.id;
+                            waypointsArray.remove(function (waypointsArray) {
+                                return waypointsArray.billboard_id === billboardId;
+                            });
+                            viewer.entities.remove(viewer.entities.getById(billboardId));
+                        }
+                    }
+                    break;
+                case 'r':
+                case 'R':
+                    renderPath();
+                    break;
+
+                case 'c':
+                case 'C':
+                    Swal.fire({
+                        title: "Clear all waypoints?",
+                        showCancelButton: true,
+                        confirmButtonText: "Process",
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            clearAll();
+                        }
+                    });
+                    break;
+            }
+        }
+    );
+
+    viewer.selectedEntityChanged.addEventListener(function(selectedEntity) {
+        if (selectedEntity) {
+            if (selectedEntity.billboard) {
+                const waypoint = waypointsArray().filter(item => item.billboard_id === selectedEntity.id)[0];
+                const billboard = viewer.entities.getById(selectedEntity.id);
+                billboard.description = `Waypoint ${waypointsArray.indexOf(waypoint)}(${waypoint.longitude.toFixed(6)}, ${waypoint.latitude.toFixed(6)}, ${waypoint.height.toFixed(2)})`;
+            }
+        }
+    });
 
 </script>
 
